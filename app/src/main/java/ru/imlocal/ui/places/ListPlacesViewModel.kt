2f2d86f2d1bdfa41c9058ce2d3d2a2ab64070ef7
Ivan <mikov_ivan@list.ru>
2020-01-15 23:00:20 +0300

    add submenu for sort

--- app/src/main/java/ru/imlocal/ui/places/ListPlacesViewModel.kt
+++ app/src/main/java/ru/imlocal/ui/places/ListPlacesViewModel.kt
@@ -1,13 +1,22 @@
 package ru.imlocal.ui.places
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import androidx.paging.PagedList
 import ru.imlocal.data.repository.NetworkState
 import ru.imlocal.data.repository.PlacePagedListRepository
+import ru.imlocal.extensions.mutableLiveData
 import ru.imlocal.models.Place
 
 class ListPlacesViewModel(private val placeRepository: PlacePagedListRepository) : ViewModel() {
+
+    private val query = mutableLiveData("")
+    private val placesList = mutableLiveData(placeRepository.fetchLivePlacePagedList())
+
+    val state: MediatorLiveData<State> = MediatorLiveData<State>().apply {
+        value = State()
+    }
+    val currentState
+        get() = state.value!!
 
     val placePagedList: LiveData<PagedList<Place>> by lazy {
         placeRepository.fetchLivePlacePagedList()
@@ -21,4 +30,24 @@ class ListPlacesViewModel(private val placeRepository: PlacePagedListRepository)
         return placePagedList.value?.isEmpty() ?: true
     }
 
+    fun handleSearch(queryStr: String?) {
+        query.value = queryStr.orEmpty()
+    }
+
+    fun handleSortMenu() {
+        updateState { it.copy(isSortOpen = !it.isSortOpen) }
+    }
+
+    fun updateState(update: (currentState: State) -> State) {
+        val updatedState: State = update(currentState)
+        state.value = updatedState
+    }
+
+    fun observeState(owner: LifecycleOwner, onChanged: (newState: State) -> Unit) {
+        state.observe(owner, Observer { onChanged(it!!) })
+    }
 }
+
+data class State(
+    val isSortOpen: Boolean = false
+)
