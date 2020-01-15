dd0bf7132f1b05f40af640f920fda25ccd23d7d5
Ivan <mikov_ivan@list.ru>
2020-01-15 22:58:01 +0300

    change listner and add menu

--- app/src/main/java/ru/imlocal/ui/places/FragmentListPlaces.kt
+++ app/src/main/java/ru/imlocal/ui/places/FragmentListPlaces.kt
@@ -1,24 +1,23 @@
 package ru.imlocal.ui.places
 
-import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_list_places.*
 import ru.imlocal.R
 import ru.imlocal.adapter.PlacePagedListAdapter
 import ru.imlocal.data.api.Api
 import ru.imlocal.data.repository.NetworkState
 import ru.imlocal.data.repository.PlacePagedListRepository
+import ru.imlocal.ui.main.FragmentMainDirections
 
 class FragmentListPlaces : Fragment() {
 
@@ -29,12 +28,45 @@ class FragmentListPlaces : Fragment() {
         fun newInstance() = FragmentListPlaces()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list_places, container, false)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            queryHint = requireContext().getString(R.string.search_place)
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    viewModel.handleSearch(query)
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+                    viewModel.handleSearch(newText)
+                    return true
+                }
+            })
+        }
+        super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        viewModel.observeState(this) {
+            renderUI(it, item)
+        }
+        when (item.itemId) {
+            R.id.sort -> viewModel.handleSortMenu()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,10 +77,16 @@ class FragmentListPlaces : Fragment() {
 
         viewModel = getViewModel()
 
-        val recyclerView: RecyclerView = view.findViewById(R.id.rv_fragment_list_places)
-        val placeAdapter = PlacePagedListAdapter(activity as Context, this)
-        recyclerView.layoutManager = LinearLayoutManager(activity)
-        recyclerView.adapter = placeAdapter
+        val placeAdapter = PlacePagedListAdapter {
+            val action = FragmentMainDirections.actionFragmentMainToFragmentVitrinaPlace(
+                it.shopId
+            )
+            NavHostFragment.findNavController(this).navigate(action)
+        }
+        with(rv_fragment_list_places) {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = placeAdapter
+        }
 
         fab.setOnClickListener { findNavController().navigate(R.id.fragment_map) }
 
@@ -66,7 +104,11 @@ class FragmentListPlaces : Fragment() {
                 placeAdapter.setNetworkState(it)
             }
         })
+    }
 
+    private fun renderUI(state: State, item: MenuItem) {
+        item.isChecked = state.isSortOpen
+        if (state.isSortOpen) sort_sub_menu_places.open() else sort_sub_menu_places.close()
     }
 
     private fun getViewModel(): ListPlacesViewModel {
